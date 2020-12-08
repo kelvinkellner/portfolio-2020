3df8d9e46f05b66c36c621f30cf3c42040ccb102
Kelvin Kellner <kelvinkellner123@gmail.com>
2020-12-07 23:36:42 -0500

    Added infinite smiley capabilities

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+let count = 0;
+
 function smileyEasterEgg() {
+    count++;
     const smiley = document.getElementById('smiley');
-    if(!smiley.classList.contains('smiley-easter-egg')) {
-        smiley.classList.add('smiley-easter-egg');
-        window.setTimeout(function () {
-            if(smiley.style.visibility = 'hidden') {
-                smiley.classList.remove('smiley-easter-egg');
-            }
-        }, 5000);
-    }
+    const clone = smiley.cloneNode(true);
+    clone.id = 'smiley-clone-' + count;
+    smiley.before(clone);
+    clone.classList.add('smiley-easter-egg');
+    window.setTimeout(function () {
+        clone.remove();
+    }, 5000);
 }
+
+// Single smiley limit code:
+//     const smiley = document.getElementById('smiley');
+//     if(!smiley.classList.contains('smiley-easter-egg')) {
+//         smiley.classList.add('smiley-easter-egg');
+//         window.setTimeout(function () {
+//             if(smiley.style.visibility = 'hidden') {
+//                 smiley.classList.remove('smiley-easter-egg');
+//             }
+//         }, 5000);
+//     }
+// }
