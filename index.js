2450cdcac467b8ffafa3aff2a15420912b21aa71
Kelvin Kellner <kelvinkellner123@gmail.com>
2020-12-07 22:11:32 -0500

    Added smiley face easter egg

---
+++ index.js
@@ -0,0 +1,11 @@
+function smileyEasterEgg() {
+    const smiley = document.getElementById('smiley');
+    if(!smiley.classList.contains('smiley-easter-egg')) {
+        smiley.classList.add('smiley-easter-egg');
+        window.setTimeout(function () {
+            if(smiley.style.visibility = 'hidden') {
+                smiley.classList.remove('smiley-easter-egg');
+            }
+        }, 4000);
+    }
+}
